d3a0b4e8fd7ecc2d81577d01de9e7c1af2ad72c6
Samuel Ardaya-Lieb <s.ardayalieb@consolinno.de>
2026-01-12 10:18:57 +0100

    gpio-conegx: bump driver version to 1.5.0

--- drivers/gpio/gpio-conegx.c
+++ drivers/gpio/gpio-conegx.c
@@ -2,7 +2,7 @@
  * @file gpio-conegx.c
  * @author S. Ardaya-Lieb (s.ardayalieb@consolinno.de)
  * @brief Driver for Consolinno Conegx Module
- * @version 1.4.2
+ * @version 1.5.0
  * 
  * @copyright: Copyrigth (c) 2021 - 2025
  * This program is free software: you can redistribute it and/or modify
